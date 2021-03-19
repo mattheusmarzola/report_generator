6d7e5a89a3470292cba2b1b940c9024eafd6cf09
Mattheus Rodrigo Marzola Leite <mattheus_marzola_5@hotmail.com>
2021-03-19 20:50:27 -0300

    adding function to generate reports from a list of files

--- lib/parser.ex
+++ lib/parser.ex
@@ -2,13 +2,13 @@ defmodule ReportsGenerator.Parser do
   def parse_file(filename) do
     "reports/#{filename}"
     |> File.stream!()
-    |> Stream.map(&parse_line(&1))
+    |> Stream.map(&parse_line(",",&1))
   end
 
-  defp parse_line(line) do
+  defp parse_line(separator, line) do
     line
     |> String.trim()
-    |> String.split(",")
+    |> String.split(separator)
     |> List.update_at(2, &String.to_integer/1)
   end
 end
--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -19,6 +19,19 @@ defmodule ReportsGenerator do
     |> Parser.parse_file()
     |> Enum.reduce(report_acc(), fn line, report -> sum_value(line, report) end)
   end
+  
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of strings"}
+  end
+
+  def build_from_many(filenames) do
+    result = 
+      filenames 
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
 
   def fetch_higher_corst(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
@@ -31,6 +44,17 @@ defmodule ReportsGenerator do
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
     %{report | "users" => users, "foods" => foods}
+  end
+
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{"foods" => foods2, "users" => users2}) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    %{"foods" => foods, "users" => users}
+  end
+
+  defp merge_maps(map1, map2) do 
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
   end
 
   defp report_acc do
